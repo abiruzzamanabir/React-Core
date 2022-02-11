d37aa52608810eb07e0a210d5cc5a7886fedf9a4
abiruzzamanabir <abiruzzamanabir17@gmail.com>
2022-02-12 02:16:43 +0600

    changed some text

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -18,7 +18,7 @@ class App extends React.Component {
             <div>
                 <SignupForm createUser={this.createUser} />
                 <div>
-                    <h3 className="my-3">All Users</h3>
+                    <h3 className="my-3">All Registered Users</h3>
                     <ul className="list-group">
                         {this.state.users.map(user => (<li key={user.id} className='list-group-item'>Name: {user.name} | Email: {user.email} | Birth Date: {user.birthDate} | Gender: {user.gender}</li>))}
                     </ul>
